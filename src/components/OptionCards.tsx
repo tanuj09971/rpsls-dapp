43ea25767e6890b58925fd8f57e7ca70d4524150
Tanuj Soni <tanuj0997@gmail.com>
2024-02-01 17:01:00 +0530

    feat: add move option cards

---
+++ src/components/OptionCards.tsx
@@ -0,0 +1,51 @@
+import { Box, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import { moves } from "@/utils/constants";
+
+const OptionCards = ({
+  setMove,
+  move,
+}: {
+  setMove: React.Dispatch<React.SetStateAction<number>>;
+  move: number;
+}) => {
+  return (
+    <Stack direction={"row"} gap={2}>
+      {moves.map((item, idx) => (
+        <Box
+          key={idx + 1}
+          sx={{
+            flexDirection: "column",
+            borderRadius: "20px",
+            height: "150px",
+            width: "150px",
+            border: "1px solid #55505052",
+            ...(move === idx + 1 && {
+              background: "#00e3ff30",
+            }),
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "pointer",
+            textTransform: "capitalize",
+            gap: 1,
+          }}
+          onClick={() => setMove(idx + 1)}
+        >
+          <Image
+            src={`/images/${item}.png`}
+            alt="img"
+            width={"60"}
+            height={"60"}
+          />
+          <Typography variant="overline" fontWeight={"bold"} color={"#737373"}>
+            {item}
+          </Typography>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default OptionCards;
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const moves = ["rock", "paper", "scissor", "spock", "lizard"];
